7e40e13499eeee9c9064e8a72b2d90f5271c3b67
matthausnawan <matthausnawan@gmail.com>
2020-02-20 17:49:54 -0300

    modulo 2 - upload de avatar

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,24 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UsersController from './app/controllers/UsersController';
+import ProvidersController from './app/controllers/ProvidersController';
+import SessionsController from './app/controllers/SessionsController';
+import FileController from './app/controllers/FileController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
-
+const upload = multer(multerConfig);
 routes.post('/users', UsersController.store);
+routes.post('/sessions', SessionsController.store);
+
+routes.use(authMiddleware);
+routes.get('/providers', ProvidersController.index);
+routes.put('/users', UsersController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
